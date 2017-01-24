c8bcc68498d980b22c69b331fe23ee28f91eef04
David <totraev@gmail.com>
2017-01-24 17:00:49 +0300

    refactor redis service

--- src/services/StorageService.ts
+++ src/services/StorageService.ts
@@ -1,26 +1,50 @@
 import { RedisClient } from 'redis'
 import * as redis from 'redis'
 import * as Bluebird from 'bluebird'
-Promise = require('bluebird')
 
 import config from '../config'
 
 const {redis: {port, host}} = config
 
+
 export interface StorageService {
-  redis: RedisClient
+  client: RedisClient
   set: (key: string, value: string) => Promise<string>
   get: (key: string) => Promise<string>
   expire: (key: string, time: number) => Promise<any>
   del: (key: string) => Promise<any>
 }
 
-const redisClient: StorageService = {
-  redis: redis.createClient(port, host),
-  set: Bluebird.promisify<string, string, string>(this.redis.set),
-  get: Bluebird.promisify<string, string>(this.redis.get),
-  expire: Bluebird.promisify<any, string, number>(this.redis.expire),
-  del: Bluebird.promisify<any, string>(this.redis.del)
+class RedisService implements StorageService {
+  client: RedisClient
+
+  constructor(client: RedisClient) {
+    this.client = client
+  }
+
+  set(key: string, value: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, (err, result) => err ? reject(err) : resolve(result))
+    })
+  }
+
+  get(key: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, result) => err ? reject(err) : resolve(result))
+    })
+  }
+
+  expire(key: string, time: number): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.client.expire(key, time, (err, result) => err ? reject(err) : resolve(result))
+    })
+  }
+
+  del(key: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err, result) => err ? reject(err) : resolve(result))
+    })
+  }
 }
 
-export default redisClient
+export default new RedisService(redis.createClient(port, host))
