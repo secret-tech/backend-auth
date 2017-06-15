44021d65f0a0b9fb50509a233e9fd4fa429542c1
Andrey Degtyaruk <hlogeon1@gmail.com>
2017-06-15 21:08:25 +0700

    Add JWT_KEY from ENV support

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const { REDIS_HOST, REDIS_PORT, PORT } = process.env
+const { REDIS_HOST, REDIS_PORT, PORT, JWT_KEY } = process.env
 
 export default {
 	app: {
@@ -11,7 +11,7 @@ export default {
 		algorithm: 'HS256',
 		secret_separator: ':',
 		expiration: 60,
-		secret: 'uZrJ!xe*xN?!;oU.u*;QOSM+|=4C?WH?6eWPcK/6AkIXIVGQguSA*r'
+		secret: JWT_KEY || 'uZrJ!xe*xN?!;oU.u*;QOSM+|=4C?WH?6eWPcK/6AkIXIVGQguSA*r'
 	},
 	redis: {
 		port: parseInt(REDIS_PORT, 10) || 6379,
