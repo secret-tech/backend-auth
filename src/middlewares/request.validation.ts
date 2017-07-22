318320daed3af211e32c21af6f58247e3c617914
Artem Pikulin <ortgma@gmail.com>
2017-07-22 19:10:34 +0700

    Closes #13. Refactor validation. Validation MWs are injected by DI.

---
+++ src/middlewares/request.validation.ts
@@ -0,0 +1,79 @@
+import * as Joi from 'joi'
+import { Response, Request, NextFunction } from 'express'
+
+export function createUser(req: Request, res: Response, next: NextFunction) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    login: Joi.string().required(),
+    sub: Joi.string().required(),
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    return res.status(422).json(result)
+  } else {
+    return next()
+  }
+}
+
+export function createTenant(req: Request, res: Response, next: NextFunction) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(6).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/),
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    return res.status(422).json(result)
+  } else {
+    return next()
+  }
+}
+
+export function loginTenant(req: Request, res: Response, next: NextFunction) {
+  const schema = Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(6).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/),
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    return res.status(422).json(result)
+  } else {
+    return next()
+  }
+}
+
+export function createToken(req: Request, res: Response, next: NextFunction) {
+  const schema = Joi.object().keys({
+    login: Joi.string().required(),
+    password: Joi.string().required(),
+    deviceId: Joi.string().required()
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    return res.status(422).json(result)
+  } else {
+    return next()
+  }
+}
+
+export function tokenRequired(req: Request, res: Response, next: NextFunction) {
+  const schema = Joi.object().keys({
+    token: Joi.string().required()
+  })
+
+  const result = Joi.validate(req.body, schema)
+
+  if (result.error) {
+    return res.status(422).json(result)
+  } else {
+    return next()
+  }
+}
