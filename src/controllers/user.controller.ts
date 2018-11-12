9b07c2b73a37603197f9523e1752bbbcc4cab73d
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-11-12 23:41:48 +0300

    List users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -40,7 +40,6 @@ export class UserController {
   )
   async listUsers(req: AuthorizedRequest, res: Response): Promise<void> {
     const result = await this.userService.listForTenant(req.tenant.id);
-    console.log("Result: ", result);
     res.status(200).send(result);
   }
 
